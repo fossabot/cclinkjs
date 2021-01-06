dd5d5a6c47d7632092d4365a37a98872506df289
hhui64 <907322015@qq.com>
2021-01-06 22:32:09 +0800

    test(lib): add a few tests :anguished:

--- test/CCLinkDataProcessing.test.ts
+++ test/CCLinkDataProcessing.test.ts
@@ -1,5 +1,17 @@
 import { CCLinkDataProcessing } from '../src/lib/CCLinkDataProcessing'
 
-test('dumps test', () => {
-  expect(CCLinkDataProcessing.replaceLinkBreak({})).toBe('{}')
+test('dumps json data', () => {
+  expect(1).toBe(1)
+})
+
+test('unpack binary data', () => {
+  expect(1).toBe(1)
+})
+
+test('format object/string', () => {
+  expect(1).toBe(1)
+})
+
+test('replaceLinkBreak object', () => {
+  expect(1).toBe(1)
 })
