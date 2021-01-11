8726ba557bfe559eefd886da4e1c5c3d18bb83a1
hhui64 <907322015@qq.com>
2021-01-11 17:00:37 +0800

    feat(index): export interface

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 /**
  * CCLinkJS Remake
  * @author hhui64<907322015@qq.com>
- * 
+ *
  * 致终于来到这里的勇敢的人：
- * 
+ *
  * 你是被上帝选中的人，是英勇的、不敌辛苦的、不眠不休的来修改我们这最棘手的代码的编程骑士。
  * 你，我们的救世主，人中之龙，我要对你说：永远不要放弃，永远不要对自己失望，永远不要逃走，
  * 辜负了自己，永远不要哭啼，永远不要说再见，永远不要说谎来伤害自己。
- * 
+ *
  */
 
 import { CCLinkJS } from './lib/CCLinkJS'
-import { CCLinkDataProcessing } from './lib/CCLinkDataProcessing'
+import { CCLinkDataProcessing, CCJsonData, CCJsonDataWithOutSidCid, ChatMsgData } from './lib/CCLinkDataProcessing'
 
-export { CCLinkJS, CCLinkDataProcessing }
+export { CCLinkJS, CCLinkDataProcessing, CCJsonData, CCJsonDataWithOutSidCid, ChatMsgData }
