fa0dd9ad822c2faa5cdde859beaf9f1a914e3a6c
hhui64 <907322015@qq.com>
2021-01-11 16:59:18 +0800

    feat(lib): middleware & event support :alien:

--- src/lib/CCLinkJS.ts
+++ src/lib/CCLinkJS.ts
@@ -8,50 +8,100 @@ interface CCLinkJSOptions {
   heartbeatInterval?: number
 }
 
+type onConnectCallbackFunction = (connection: WebSocket.connection) => void
+type onErrorCallbackFunction = (error: Error) => void
+type onCloseCallbackFunction = (code: number, desc: string) => void
+type onMessageCallbackFunction = (data: WebSocket.IMessage) => void
+
 /**
  * cclink.js 主类
  */
 class CCLinkJS {
-  WebSocket: {
+  public WebSocket: {
     client: WebSocket.client
     server: WebSocket.server
     socketConnection: WebSocket.connection | null
   }
-  cfg: { url: string; useWss: boolean }
+  public cfg: { url: string; useWss: boolean }
+  private _event: {
+    connect?: onConnectCallbackFunction
+    error?: onErrorCallbackFunction
+    close?: onCloseCallbackFunction
+    message?: onMessageCallbackFunction
+  }
   private _heartbeatInterval: NodeJS.Timeout | null = null
-  private _listenQueue: Function[]
+  private middleware: Function[]
   constructor(options?: CCLinkJSOptions) {
     this.cfg = {
       url: '//weblink.cc.163.com/',
       useWss: true,
     }
+
     this.WebSocket = {
       client: new WebSocket.client(),
       server: new WebSocket.server(),
       socketConnection: null,
     }
-    this._listenQueue = []
+
+    this._event = {
+      connect: undefined,
+      error: undefined,
+      close: undefined,
+      message: undefined,
+    }
+
+    this.middleware = []
   }
 
   /**
    * 连接服务器
    */
-  public connect(): void {
+  public connect(): this {
     this.WebSocket.client.connect((this.cfg.useWss ? 'wss:' : 'ws:') + this.cfg.url)
     this.WebSocket.client.on('connect', (connection: WebSocket.connection) => {
       this._onConnect(connection)
-      connection.on('error', (error: Error) => {
-        this._onError(error)
-      })
-      connection.on('close', (code: number, desc: string) => {
-        this._onClose(code, desc)
-      })
-      connection.on('message', (data: WebSocket.IMessage) => {
-        if (data.type === 'binary') {
-          this._onMessage(data)
-        }
-      })
+      connection
+        .on('error', (error: Error) => {
+          this._onError(error)
+        })
+        .on('close', (code: number, desc: string) => {
+          this._onClose(code, desc)
+        })
+        .on('message', (data: WebSocket.IMessage) => {
+          if (data.type === 'binary') {
+            this._onMessage(data)
+          }
+        })
     })
+
+    return this
+  }
+
+  /**
+   * 设置事件回调
+   * @param event 事件名称
+   * @param callback 回调方法
+   */
+  public on(event: 'connect', callback?: (connection: WebSocket.connection) => void): this
+  public on(event: 'error', callback?: (error: Error) => void): this
+  public on(event: 'close', callback?: (code: number, desc: string) => void): this
+  public on(event: 'message', callback?: (data: WebSocket.IMessage) => void): this
+  public on(event: string, callback?: Function): this {
+    switch (event) {
+      case 'connect':
+        this._event.connect = <onConnectCallbackFunction>callback
+        break
+      case 'error':
+        this._event.error = <onErrorCallbackFunction>callback
+        break
+      case 'close':
+        this._event.close = <onCloseCallbackFunction>callback
+        break
+      case 'message':
+        this._event.message = <onMessageCallbackFunction>callback
+        break
+    }
+    return this
   }
 
   /**
@@ -60,6 +110,7 @@ class CCLinkJS {
    */
   private _onConnect(connection: WebSocket.connection): void {
     this.WebSocket.socketConnection = connection
+    this._event.connect && this._event.connect(connection)
     this._startHeartBeat()
   }
 
@@ -67,7 +118,9 @@ class CCLinkJS {
    * 连接错误处理方法
    * @param {Error} error
    */
-  private _onError(error: Error): void {}
+  private _onError(error: Error): void {
+    this._event.error && this._event.error(error)
+  }
 
   /**
    * 连接关闭处理方法
@@ -76,6 +129,7 @@ class CCLinkJS {
    */
   private _onClose(code: number, desc: string): void {
     this.WebSocket.socketConnection = null
+    this._event.close && this._event.close(code, desc)
     this._stopHeartBeat()
   }
 
@@ -87,6 +141,11 @@ class CCLinkJS {
     if (data.binaryData?.byteLength) {
       let Uint8ArrayData = new Uint8Array(data.binaryData),
         unpackData = CCLinkDataProcessing.unpack(Uint8ArrayData).format('json')
+
+      this._event.message && this._event.message(data)
+
+      const fn = this.compose(this.middleware)
+      fn(unpackData)
     }
   }
 
@@ -97,17 +156,9 @@ class CCLinkJS {
    */
   public send(data: CCJsonData): void {
     let Uint8ArrayData: Uint8Array = new CCLinkDataProcessing(data).dumps(),
-      BufferData: Buffer = Buffer.from(Uint8ArrayData.buffer)
+      BufferData: Buffer = Buffer.from(Uint8ArrayData)
     this.WebSocket.socketConnection && this.WebSocket.socketConnection.sendBytes(BufferData)
   }
-
-  /**
-   * @TODO 监听指定接口消息
-   * @param ccsid
-   * @param cccid
-   * @param options
-   */
-  public listen(ccsid: number, cccid: number, options?: object): void {}
 
   /**
    * 开始发送心跳包
@@ -130,6 +181,48 @@ class CCLinkJS {
    */
   private _stopHeartBeat(): void {
     this._heartbeatInterval && clearInterval(this._heartbeatInterval)
+  }
+
+  /**
+   * 使用中间件
+   * @param {Function} fn callback function
+   */
+  public use(fn: (data: CCJsonData, next: Function) => void): this {
+    if (typeof fn !== 'function') {
+      throw new TypeError('middleware must be a function!')
+    } else {
+      this.middleware.push(fn)
+    }
+
+    return this
+  }
+
+  private compose(middleware: Function[]) {
+    if (!Array.isArray(middleware)) throw new TypeError('Middleware stack must be an array!')
+    for (const fn of middleware) {
+      if (typeof fn !== 'function') throw new TypeError('Middleware must be composed of functions!')
+    }
+
+    return function (data: CCJsonData, next?: Function) {
+      let index = -1
+      return dispatch(0)
+      function dispatch(i: number) {
+        if (i <= index) return Promise.reject(new Error('next() called multiple times'))
+        index = i
+        let fn = middleware[i]
+        if (i === middleware.length && next) fn = next
+        if (!fn) return Promise.resolve()
+        try {
+          return Promise.resolve(
+            fn(data, function next() {
+              return dispatch(i + 1)
+            })
+          )
+        } catch (err) {
+          return Promise.reject(err)
+        }
+      }
+    }
   }
 }
 
